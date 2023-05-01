1284c4570429bc24f9bceda42f685ecb7f8ae57d
Horacio Camacho <camacho19992012@gmail.com>
2023-05-01 12:35:44 -0500

    Se retiran paquetes no necesarios para el auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,12 @@ import * as process from 'process';
 import { AuthModule } from './auth/auth.module';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { MailModule } from './mail/mail.module';
 import { LoggerFactory } from 'typeorm/logger/LoggerFactory';
-import { MailService } from './mail/mail.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
-    MailModule,
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -26,28 +23,8 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true,
       synchronize: true,
     }),
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.MAIL_HOST,
-        port: 587,
-        auth: {
-          user: process.env.MAILGUN_USERNAME,
-          pass: process.env.MAILGUN_PASSWORD,
-        },
-      },
-      defaults: {
-        from: '"NestJS" <sender@example.com>',
-      },
-      template: {
-        dir: __dirname + '/mail/templates/',
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
-      },
-    }),
   ],
   controllers: [AppController],
-  providers: [AppService, LoggerFactory, MailService],
+  providers: [AppService, LoggerFactory],
 })
 export class AppModule {}
